73ec00d8e384fe59b78a3242c57c80e1b4e14396
Fujimura Daisuke <me@fujimuradaisuke.com>
2012-10-31 12:20:01 +0900

    InstanceMethods is deprecated

--- authentication.rb
+++ authentication.rb
@@ -4,46 +4,41 @@ class Unauthorized < StandardError; end
 module Authentication
 
   extend ActiveSupport::Concern
+
   included do |base|
     base.class_eval do
       helper_method :current_user
     end
   end
 
-  module ClassMethods
+  # Store user id in session.
+  #
+  def login!(user)
+    raise Unauthorized unless user
+    session[:current_user_id] = user.id
   end
 
-  module InstanceMethods
+  # Return current_user.
+  # If it does not exist, returns nil.
+  #
+  # @return user or nil
+  def current_user
+    @current_user ||= ::User.find(session[:current_user_id])
+  rescue ActiveRecord::RecordNotFound
+    nil
+  end
 
-    # Store user id in session.
-    #
-    def login!(user)
-      raise Unauthorized unless user
-      session[:current_user_id] = user.id
-    end
+  # Return current_user exists or not.
+  #
+  # @return [Boolean] Whether or not page's plan is updated.
+  def logged_in?
+    !current_user.blank?
+  end
 
-    # Return current_user.
-    # If it does not exist, returns nil.
-    #
-    # @return user or nil
-    def current_user
-      @current_user ||= ::User.find(session[:current_user_id])
-    rescue ActiveRecord::RecordNotFound
-      nil
-    end
-
-    # Return current_user exists or not.
-    #
-    # @return [Boolean] Whether or not page's plan is updated.
-    def logged_in?
-      !current_user.blank?
-    end
-
-    # Delete current_user from database and session.
-    #
-    def logout!
-      return unless current_user
-      @current_user = session[:current_user_id] = nil
-    end
+  # Delete current_user from database and session.
+  #
+  def logout!
+    return unless current_user
+    @current_user = session[:current_user_id] = nil
   end
 end
