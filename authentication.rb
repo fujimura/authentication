3adc608860bdb6b830fe240a4183097a40b9cf0e
Fujimura Daisuke <me@fujimuradaisuke.com>
2012-10-31 14:41:33 +0900

    Refactor and add spec

--- authentication.rb
+++ authentication.rb
@@ -1,20 +1,15 @@
 module Authentication
 
+  # Expects #session and #find_current_user to be exist.
+
   class CurrentUserDoesNotExist < StandardError ; end
   class Unauthenticated < StandardError; end
-
-  extend ActiveSupport::Concern
-
-  included do |base|
-    base.class_eval do
-      helper_method :current_user
-    end
-  end
 
   # Store user id in session.
   #
   def login!(user)
     raise Unauthenticated unless user
+    @current_user = user
     session[:current_user_id] = user.id
   end
 
@@ -23,16 +18,14 @@ module Authentication
   #
   # @return user or nil
   def current_user
-    @current_user ||= ::User.find(session[:current_user_id])
-  rescue ActiveRecord::RecordNotFound
-    nil
+    @current_user ||= find_current_user
   end
 
   # Return current_user exists or not.
   #
-  # @return [Boolean] Whether or not page's plan is updated.
+  # @return [Boolean]
   def logged_in?
-    !current_user.blank?
+    not current_user.nil?
   end
 
   # Delete current_user from database and session.
---
+++ spec.rb
@@ -0,0 +1,78 @@
+require 'rspec'
+require './authentication'
+
+
+class Controller
+  include Authentication
+  attr_accessor :session
+
+  def initialize
+    self.session = {}
+  end
+
+  def find_current_user success = true
+    if session[:current_user_id]
+      true
+    else
+      nil
+    end
+  end
+end
+
+class User
+  def initialize(attributes)
+    @attributes = attributes
+  end
+  def id
+    @attributes[:id]
+  end
+end
+
+describe Authentication do
+  let(:controller) { Controller.new }
+  let(:user)       { User.new(id: 300) }
+
+  describe '#login!' do
+    context 'with nil' do
+      it "should raise Unauthenticated" do
+        expect do
+          controller.login! nil
+        end.to raise_error Authentication::Unauthenticated
+      end
+    end
+  end
+
+  describe '#current_user' do
+    context 'when logged in' do
+      it "should return user" do
+        controller.login! user
+        controller.current_user.should == user
+      end
+    end
+    context 'when not logged in' do
+      it "should call #find_current_user and return its result" do
+        controller.should_receive(:find_current_user).and_return "something"
+        controller.current_user.should == "something"
+      end
+    end
+  end
+  describe '#logged_in?' do
+    context 'when logged in' do
+      it "should return true" do
+        controller.login! user
+        controller.logged_in?.should == true
+      end
+    end
+  end
+
+  describe '#logout!' do
+    context 'when logged in' do
+      it "should set current_user to nil" do
+        controller.login! user
+        controller.logout!
+        controller.current_user.should == nil
+        controller.session[:current_user_id].should == nil
+      end
+    end
+  end
+end
